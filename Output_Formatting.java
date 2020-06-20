f1d7ca167fecf67c6b6bbf37f3f0f80813f52621
Mohit Kumar <singla45567@gmail.com>
2020-06-20 12:15:15 +0530

    updating code

--- Output_Formatting.java
+++ Output_Formatting.java
@@ -54,3 +54,27 @@ public class Solution {
 ---------------------------------------------Solution---------------------------------------------------------------
 */
 
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                  System.out.printf("%-15s%03d%n",s1,x);
+                /*
+                ("%-15s%03d%n", s1, x) % : used as a formatter. '-' : minus sign used for left indentation of the string. 
+                15s : here 15 denotes the string's minimum field width 15. '0' : pads the extra 0s in the integer. 
+                3d : here 3 denotes integer's minimum field width 3. %n : prints the new line.
+                */
+            }
+            System.out.println("================================");
+
+    }
+}
+
