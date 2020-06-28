a78a30130cd5738a39174b0c13d8239fad500a46
Mohit Kumar <singla45567@gmail.com>
2020-06-28 18:31:37 +0530

    Create Java_Date_and_Time.java

---
+++ Java_Date_and_Time.java
@@ -0,0 +1,90 @@
+/*
+__________________________________________________________________Problem Statement_____________________________________________________________________
+
+Problem Statement link: https://www.hackerrank.com/challenges/java-date-and-time/problem
+
+the Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
+
+You are given a date. You just need to write the method, , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
+
+For example, if you are given the date , the method should return  as the day on that date.
+
+image
+
+Input Format
+
+A single line of input containing the space separated month, day and year, respectively, in    format.
+
+Constraints
+
+Output Format
+
+Output the correct day in capital letters.
+
+Sample Input
+
+08 05 2015
+Sample Output
+
+WEDNESDAY
+Explanation
+
+The day on August th  was WEDNESDAY.
+
+__________________________________________________________________________Solution____________________________________________________________
+*/
+
+port java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.time.LocalDate;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+    return LocalDate.of(year, month, day).getDayOfWeek().name();
+     //   return S;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
