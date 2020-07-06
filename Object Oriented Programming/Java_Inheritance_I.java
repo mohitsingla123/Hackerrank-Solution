ea46e2286fae0a5fad755825cb06b86084c8889e
Mohit Kumar <singla45567@gmail.com>
2020-07-06 10:13:36 +0530

    Create Java_Inheritance_I.java

---
+++ Object Oriented Programming/Java_Inheritance_I.java
@@ -0,0 +1,86 @@
+/*
+___________________________________________________________________Problem  Statement_____________________________________________________
+
+problem Statement link: hackerrank.com/challenges/java-inheritance-1/problem
+
+Using inheritance, one class can acquire the properties of others. Consider the following Animal class:
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+This class has only one method, walk. Next, we want to create a Bird class that also has a fly method. We do this using extends keyword:
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+}
+Finally, we can create a Bird object that can both fly and walk.
+
+public class Solution{
+   public static void main(String[] args){
+
+      Bird bird = new Bird();
+      bird.walk();
+      bird.fly();
+   }
+}
+The above code will print:
+
+I am walking
+I am flying
+This means that a Bird object has all the properties that an Animal object has, as well as some additional unique properties.
+
+The code above is provided for you in your editor. You must add a sing method to the Bird class, then modify the main method accordingly so that the code prints the following lines:
+
+I am walking
+I am flying
+I am singing
+
+
+_________________________________________________________ Solution________________________________________________
+
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk()
+	{
+		System.out.println("I am walking");
+	}
+}
+class Bird extends Animal
+{
+	void fly()
+	{
+		System.out.println("I am flying");
+	}
+    void walk()
+    {
+        System.out.println("I am walking");
+    }
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution{
+
+   public static void main(String args[]){
+
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
+}
