af89f2f0e5c7dd9c696a87239d5487c43066a259
Mohit Kumar <singla45567@gmail.com>
2020-08-22 19:13:10 +0530

    Create VeryBigSum.java

---
+++ Problem Statement/VeryBigSum.java
@@ -0,0 +1,97 @@
+/*
+Problem Statement Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+Problem Statement: 
+
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+Return
+
+int: the sum of all array elements
+Input Format
+
+The first line of the input consists of an integer .
+The next line contains  space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+
+Constraints
+
+
+Sample Input
+
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+Output
+
+5000000015
+Note:
+
+The range of the 32-bit integer is .
+When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
+
+
+
+
+
+Solution:
+All test cases passed.
+*/
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long aVeryBigSum(long[] ar) {
+long sum=0;
+for(int i=0;i<ar.length;i++)
+{
+sum = sum+ ar[i];
+}
+return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
