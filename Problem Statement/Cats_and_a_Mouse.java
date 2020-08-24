34874ee898ed9c77523f54c26b28e3c29cd1fc60
Mohit Kumar <singla45567@gmail.com>
2020-08-24 20:58:38 +0530

    Update Cats_and_a_Mouse.java

--- Problem Statement/Cats_and_a_Mouse.java
+++ Problem Statement/Cats_and_a_Mouse.java
@@ -57,11 +57,73 @@ Because the mouse escapes, we print Mouse C on a new line.
 
 
 
-
-
-
-
-
 Solution:
 All test cases passed
 */
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static String catAndMouse(int x, int y, int z) {
+String p="";
+int a = x-z;
+int b = y-z;
+if(a<0)
+{
+    a = a*(-1);
+}
+if(b<0)
+{
+    b = b*(-1);
+}
+if(a==b)
+{
+    p="Mouse C";
+}
+else if(a<b)
+{
+    p="Cat A";
+}
+else
+{
+    p="Cat B";
+}
+return p;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] xyz = scanner.nextLine().split(" ");
+
+            int x = Integer.parseInt(xyz[0]);
+
+            int y = Integer.parseInt(xyz[1]);
+
+            int z = Integer.parseInt(xyz[2]);
+
+            String result = catAndMouse(x, y, z);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
